f520cd42c363f99d66999415e1c834d513e3e53f
Fremen1990 <fremen1990@gmail.com>
2022-01-28 15:33:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const {Restaurant} = require('./restaurant');
+
+const megaRestaurant = new Restaurant();
+let tablesCount = 25;
+// Tutaj dodaj nasłuchiwanie
+
+megaRestaurant
+    .on("OpenRestaurant", () => console.log("RESTAURANT OPEN!"))
+    .on("closeRestaurant", () => console.log("RESTAURANT CLOSEED"))
+    .on('tablesCountUpdate',  change => {
+        tablesCount += change;
+        console.log(`Dostepnych stolikow ${tablesCount}`)
+    });
+
+// .on("takeTableWithoutReservation", ww=>(tablesCount += ww))
+// .on("takeTableWithoutReservation", ww=>(tablesCount - ww))
+// .on("reserveTable", rt=>(tablesCount-rt))
+// .on("cancelTableReservation", cr=>tablesCount+cr)
+// .on("reserveTable", rt=>(tablesCount-rt))
+// .on("reserveTable", rt=>(tablesCount-rt))
+// .on("takeTableWithoutReservation", ww=>(tablesCount - ww))
+// .on("takeTableWithoutReservation", ww=>(tablesCount - ww))
+// .on("cleanupTable", ct=>(tablesCount - ct))
+
+
+// console.log(tablesCount)
+
+megaRestaurant.open(); // "Otwarto restaurację."
+
+megaRestaurant.takeTableWithoutReservation(); // "Dostepnych stolików: 24."
+
+megaRestaurant.takeTableWithoutReservation(); // "Dostepnych stolików: 23."
+
+megaRestaurant.reserveTable(); // "Dostepnych stolików: 22."
+
+megaRestaurant.cancelTableReservation(); // "Dostepnych stolików: 23."
+
+megaRestaurant.reserveTable(); // "Dostepnych stolików: 22."
+
+megaRestaurant.reserveTable(); // "Dostepnych stolików: 21."
+
+megaRestaurant.takeTableWithoutReservation(); // "Dostepnych stolików: 20."
+
+megaRestaurant.takeTableWithoutReservation(); // "Dostepnych stolików: 19."
+
+megaRestaurant.cleanupTable(); // "Dostepnych stolików: 20."
+
+megaRestaurant.close(); // "Zamknięto restaurację."
